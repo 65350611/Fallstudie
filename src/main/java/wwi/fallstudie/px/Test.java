7421fcd6ba2be0c0146e42cc8e96a59ac10dead1
jonathan <jonathan.smith94@gmx.de>
2019-09-24 11:15:16 +0200

    First Commit without any classes beside Test Class

---
+++ src/main/java/wwi/fallstudie/px/Test.java
@@ -0,0 +1,13 @@
+package wwi.fallstudie.px;
+
+/**
+ * Hello world!
+ *
+ */
+public class Test 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
