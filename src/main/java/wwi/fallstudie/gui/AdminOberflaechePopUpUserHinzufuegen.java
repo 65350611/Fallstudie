a5bce823ccf8b0d5a127aa706031032f785ef175
nicojose <nicojose.neitzert@gmail.com>
2019-10-09 17:49:13 +0200

    gui erweitert admin popup

--- src/main/java/wwi/fallstudie/gui/AdminOberflaechePopUpUserHinzufuegen.java
+++ src/main/java/wwi/fallstudie/gui/AdminOberflaechePopUpUserHinzufuegen.java
@@ -2,12 +2,68 @@ package wwi.fallstudie.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class AdminOberflaechePopUpUserHinzufuegen extends JFrame {
+
+    private JTextField usernameField;
+    private JButton hinzufuegen;
+    private JButton abbrechen;
 
     public AdminOberflaechePopUpUserHinzufuegen(){
         super("Nutzer hinzufügen");
 
-        setLayout(new GridBagLayout());
+        setLayout(new GridBagLayout()); //set Layout Manager
+
+        //initialise variables
+        usernameField = new JTextField();
+        hinzufuegen = new JButton();
+        abbrechen = new JButton();
+
+
+        //edit usernameField
+        usernameField.setForeground(Color.LIGHT_GRAY);
+        usernameField.setText("Nutzername:");
+        usernameField.addFocusListener(new FocusAdapter() {
+            public void focusGained(FocusEvent e) {
+                JTextField source = (JTextField)e.getComponent();
+                source.setText("");
+                source.setForeground(Color.BLACK);
+                source.removeFocusListener(this);
+            }
+        });
+
+         /*
+        setting variables to Layout
+         */
+        GridBagConstraints gc = new GridBagConstraints();
+
+        gc.weightx = 1;
+        gc.weighty = 1;
+
+        ////////////////////// reihe 1 ///////////////////////
+
+        //usernameField
+        gc.gridx = 0;
+        gc.gridy = 0;
+        add(usernameField, gc);
+
+        ////////////////////// reihe 2 ///////////////////////
+
+        //hinzufuegen
+        gc.gridx = 0;
+        gc.gridy = 1;
+        add(hinzufuegen, gc);
+
+        //abbrechen
+        gc.gridx = 1;
+        gc.gridy = 1;
+        add(abbrechen, gc);
+
+        //set JFrame data
+        setSize(320, 100);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setVisible(true);
     }
 }
