e6d7b0608185b66b01150ddf0d0298a80feeb8b3
nicojose <nicojose.neitzert@gmail.com>
2019-10-10 12:36:53 +0200

    mein Akku stirbt :)

--- src/main/java/wwi/fallstudie/App.java
+++ src/main/java/wwi/fallstudie/App.java
@@ -3,6 +3,9 @@ package wwi.fallstudie;
 import wwi.fallstudie.gui.AdminOberflaechePopUpPasswortUserNeuSetzen;
 import wwi.fallstudie.gui.AdminOberflaechePopUpUserHinzufuegen;
 import wwi.fallstudie.gui.Login;
+import wwi.fallstudie.gui.MessagePopup;
+
+import javax.swing.*;
 
 public class App {
     public static void main(String[] args){
