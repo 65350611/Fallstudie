6040fafc28b90bb1a2e777a1dfd880db38609cab
jonathan <jonathan.smith94@gmx.de>
2019-10-16 15:59:05 +0200

    ..

--- src/main/java/backend/Logik.java
+++ src/main/java/backend/Logik.java
@@ -2,6 +2,7 @@ package backend;
 
 import haushaltsbuch.ausgaben;
 import haushaltsbuch.dbConnector;
+import wwi.fallstudie.database.DbAbfragen;
 import wwi.fallstudie.pojos.AdmPojo;
 import wwi.fallstudie.pojos.UserPojo;
 
@@ -13,11 +14,9 @@ public class Logik {
 	private Logik() {
 
 	}
-	public void ausgabenAnzeigen() {
-		
-		ausgaben ag = con.zeigeAusgaben(usr.getName());
-		
-		
+	
+	public static void nutzerAnlegen(int rolle, String userName, String pwd) {
+		DbAbfragen.
 	}
 	
 	
