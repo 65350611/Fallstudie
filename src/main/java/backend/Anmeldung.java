d73c2215ec61c62972f322f852202c728800361f
jonathan <jonathan.smith94@gmx.de>
2019-10-16 15:51:59 +0200

    ..

--- src/main/java/backend/Anmeldung.java
+++ src/main/java/backend/Anmeldung.java
@@ -5,7 +5,7 @@ import wwi.fallstudie.database.DbAbfragen;
 public interface Anmeldung {
 
 	public static void anmelden(String userName, String pwd) {
-		if ( pwd.contentEquals(DbAbfragen.zeigeNutzer(userName).toString())){
+		if ( pwd.contentEquals(DbAbfragen.gibPasswort(userName).toString())){
 			
 			
 			if (DbAbfragen.gibRolle(userName)== 1) { 
