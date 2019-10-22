36686ddff0d9b9e231142d131f797c84833dc6b4
jonathan <jonathan.smith94@gmx.de>
2019-10-22 11:53:30 +0200

    ..

---
+++ src/main/java/backend_exceptions/FalscheAdmPwdAendernMethodeException.java
@@ -0,0 +1,8 @@
+package backend_exceptions;
+
+public class FalscheAdmPwdAendernMethodeException extends Exception {
+	public FalscheAdmPwdAendernMethodeException() {
+		super("Falsche Methode");
+	}
+
+}
