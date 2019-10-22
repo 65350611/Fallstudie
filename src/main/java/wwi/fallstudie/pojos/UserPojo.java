ee3410c71afdac446498cb7e04cf68331b28cff7
jonathan <jonathan.smith94@gmx.de>
2019-10-22 11:20:30 +0200

    ..

--- src/main/java/wwi/fallstudie/pojos/UserPojo.java
+++ src/main/java/wwi/fallstudie/pojos/UserPojo.java
@@ -2,24 +2,13 @@ package wwi.fallstudie.pojos;
 
 public class UserPojo {
 	private String name;
-	
 
 	public UserPojo(String name) {
 		this.name = name;
 	}
 
-
-
-
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-
-	
 }
