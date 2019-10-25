2184b174f3c566f1d389d8d008e184d226a6fc0e
FloHoheneder <wi18068@lehre.dhbw-stuttgart.de>
2019-10-25 13:24:50 +0200

    Nur Kommentare

--- src/main/java/wwi/fallstudie/database/testNutzer.java
+++ src/main/java/wwi/fallstudie/database/testNutzer.java
@@ -8,6 +8,8 @@ public class testNutzer {
 		
 		DbAbfragen.baueVerbindungAuf();
 		
+		//1: SELECT Statements - Abfragen
+		
 		//System.out.println(DbAbfragen.gibRolle("Flo"));
 		//System.out.println(DbAbfragen.gibNutzer());
 		
@@ -19,16 +21,22 @@ public class testNutzer {
 		//System.out.println(DbAbfragen.gibAusgabenFuerZeitraumMit("Frodo", "2000-01-01", "2020-01-01"));
 		//System.out.println(DbAbfragen.gibAusgabenFuerKategorie("Jonny", 3));
 		//System.out.println(DbAbfragen.gibAusgabenMitKategorienamen("Frodo"));
+		
+		//2: CREATE Statements - neue Einträge anlegen
 				
 		//System.out.println(DbAbfragen.neuerNutzer(1, "1111", "tester4"));
 		//System.out.println(DbAbfragen.neueKategorie("testKat6"));
 		//System.out.println(DbAbfragen.neueAusgabe("testAusgabe17", "Uni", 1, 17f, "2015-10-12"));
 		
-		//System.out.println(DbAbfragen.aenderePasswort("Nico", "niconico"));
+		//3: UPDATE Statements - Einträge ändern
+		
+		//System.out.println(DbAbfragen.aenderePasswort("Sauron", "niconico"));
 		//System.out.println(DbAbfragen.aendereKategorieDerAusgabenAufSonstige("tester2", 40));
 		//System.out.println(DbAbfragen.aendereKategorieDerAusgaben("Urlaub", "tester2", 26));
-		//System.out.println(DbAbfragen.aendereAusgabe("testAusgabeXXX", 2, 121212f, sqlDate, "Jonny", 12));
+		//System.out.println(DbAbfragen.aendereAusgabe("testAusgabe", 2, 121212f, sqlDate, "Jonny", 12));
 		//System.out.println(DbAbfragen.aendereKategorieDerAusgabenAufNeu("Auto", "tester2", 3));
+		
+		//4: DELETE Statements - Einträge löschen
 		
 		//System.out.println(DbAbfragen.loescheKategorie("Flo", 3));
 		//System.out.println(DbAbfragen.loescheNutzer("tester3"));
